4b2f1712a77395f98fcdee992150040be662129e
Alex Brashear <alexbrashear@gmail.com>
2021-02-13 12:46:36 -0500

    Added ability to retrieve a single person's information by passing in name as a param. Renamed get /person to /people to provide clarity and prevent conflict with new call

--- api/person.rb
+++ api/person.rb
@@ -4,8 +4,16 @@ class Persons < Grape::API
   version 'v1'
   format :json
 
+  people = DB[:people]
+
+  get '/people' do
+    people.map([:id, :name])
+  end
+
+  params do
+    requires :name, type: String, desc: "Name of the person"
+  end
   get '/person' do
-    people = DB[:people]
-    { people: people.map([:id, :name]) }
+    people.where(:name => params[:name]).first.to_json
   end
 end
