ffdb65535b9fe1e11870acf523d8f4c2e0be0d12
Alex Brashear <alexbrashear@gmail.com>
2021-02-13 21:00:45 -0500

    Endpoint to retrieve consumption streaks has been implemented

--- api/consumption.rb
+++ api/consumption.rb
@@ -26,6 +26,26 @@ class Consumption < Grape::API
     consumption_data.all
   end
 
+    # Endpoint to get all consumption record streaks where more pizza was consumed the next day than the previous
+  get '/consumptionStreaks' do
+    date_count = consumption.group_and_count(:date).order(:date)
+    previousValue = date_count.first[:count]
+    streaks = []
+    newStreak = [date_count.first[:date]]
+    date_count.map { |count|
+      if (count[:count] > previousValue) 
+        newStreak.push(count[:date])
+      else
+        if newStreak.length() > 1
+          streaks.push(newStreak)
+        end
+        newStreak = [count[:date]]
+      end
+      previousValue = count[:count]
+    }
+    streaks
+  end
+
   # Endpoint to get all consumption records for which day the most pizzas were consumed in each month
   # TODO: Figure out a way to make this cleaner. Currently doesn't check to see if dates with the same month are in the same year, and requires checking each month, even if there are no records for that month
   get '/consumptionByMonth' do
